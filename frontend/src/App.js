c76cd998186349374e6673b40bef7bd0c45340f4
Yuliia <julia.novak9@gmail@com>
2023-02-01 21:22:02 +0100

    add newsletter signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import EditEventPage from "./pages/EditEventPage";
-import EventDetailPage, {loader as eventsDetailsLoader, action as deleteEventAction} from "./pages/EventDetailPage";
+import EventDetailPage, {
+   loader as eventsDetailsLoader,
+   action as deleteEventAction,
+} from "./pages/EventDetailPage";
 import EventsPage from "./pages/EventsPage";
 import NewEventPage from "./pages/NewEventPage";
 import HomePage from "./pages/HomePage";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
-import {loader as eventsLoader} from './pages/Events'
+import { loader as eventsLoader } from "./pages/Events";
 import ErrorPage from "./pages/Error";
-import {action as manipulateEventAction} from './components/EventForm'
+import { action as manipulateEventAction } from "./components/EventForm";
+import NewsletterPage from "./pages/Newsletter";
+import { action as newsletterAction } from "./pages/Newsletter";
 
 const router = createBrowserRouter([
    {
       path: "/",
       element: <RootLayout />,
-      errorElement: <ErrorPage/>,
+      errorElement: <ErrorPage />,
       children: [
          { index: true, element: <HomePage /> },
          {
@@ -27,21 +32,33 @@ const router = createBrowserRouter([
                   loader: eventsLoader,
                },
                {
-                  path: ':eventId',
-                  id: 'event-detail',
+                  path: ":eventId",
+                  id: "event-detail",
                   loader: eventsDetailsLoader,
                   children: [
-                     { 
-                        index: true, 
-                        element: <EventDetailPage />, 
+                     {
+                        index: true,
+                        element: <EventDetailPage />,
                         action: deleteEventAction,
                      },
-                     { path: "edit", element: <EditEventPage />, action: manipulateEventAction },
-                  ]
+                     {
+                        path: "edit",
+                        element: <EditEventPage />,
+                        action: manipulateEventAction,
+                     },
+                  ],
                },
-              
-               { path: "new", element: <NewEventPage />, action: manipulateEventAction},
+               {
+                  path: "new",
+                  element: <NewEventPage />,
+                  action: manipulateEventAction,
+               },
             ],
+         },
+         {
+            path: "newsletter",
+            element: <NewsletterPage />,
+            action: newsletterAction,
          },
       ],
    },
