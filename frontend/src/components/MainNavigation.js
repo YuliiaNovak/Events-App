e5f8cfaf6f4beb3146ca834e2521e9222cf9391b
Yuliia <julia.novak9@gmail@com>
2023-02-02 14:24:06 +0100

    add authentication setup

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -39,6 +39,16 @@ function MainNavigation() {
                      Newsletter
                   </NavLink>
                </li>
+               <li>
+                  <NavLink
+                     to="auth"
+                     className={({ isActive }) =>
+                        isActive ? classes.active : undefined
+                     }
+                  >
+                     Sign in
+                  </NavLink>
+               </li>
             </ul>
          </nav>
          <NewsletterSignup />
