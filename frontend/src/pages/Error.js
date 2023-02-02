447d82f4d6cdb21841207c856f07bf3a68287405
Yuliia <julia.novak9@gmail@com>
2023-02-02 13:28:25 +0100

    add PageContent to HomePage

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,30 +1,30 @@
-import { useRouteError } from "react-router-dom"
-import MainNavigation from "../components/MainNavigation"
-import PageContent from "./PageContent"
+import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+import PageContent from "../components/PageContent";
 
 const ErrorPage = () => {
-  const error = useRouteError()
+   const error = useRouteError();
 
-  let title='An error occurred!'
-  let message='Something went wrong.'
+   let title = "An error occurred!";
+   let message = "Something went wrong.";
 
-  if(error.status === 500){
-    message = JSON.parse(error.data).message
-  }
+   if (error.status === 500) {
+      message = JSON.parse(error.data).message;
+   }
 
-  if (error.status===404){
-    title='Not found!'
-    message = 'Could not find resource or page.'
-  }
+   if (error.status === 404) {
+      title = "Not found!";
+      message = "Could not find resource or page.";
+   }
 
-  return ( 
-    <>
-      <MainNavigation/>
-        <PageContent title={title}>
-          <p>{message}</p>
-        </PageContent>
-    </>
-  )
-}
+   return (
+      <>
+         <MainNavigation />
+         <PageContent title={title}>
+            <p>{message}</p>
+         </PageContent>
+      </>
+   );
+};
 
-export default ErrorPage
+export default ErrorPage;
